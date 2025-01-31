e77f5835358a57ca4b9858e8fe1556b5ebf0bc7f
Khanh Nguyen <cammini@khanhs-mac-mini.lan>
2025-01-30 21:28:02 -0500

    edit delete button style

--- src/pages/Edit/edit.jsx
+++ src/pages/Edit/edit.jsx
@@ -1,35 +1,32 @@
+import { createSignal, For, createEffect } from "solid-js";
 import "./Edit.css";
+
 const Edit = () => {
+  const [workouts, setWorkouts] = createSignal([
+    { name: "push ups", seconds: 30, pos: 0 },
+    { name: "pull ups", seconds: 20, pos: 11 },
+    { name: "plank", seconds: 10, pos: 2 },
+  ]);
   return (
     <>
       <ul class="todo-list">
-        <li>
-          <div class="todo-header">
-            <span>Buy groceries</span>
-            <i class="fas fa-trash trash-icon"></i>
-          </div>
-          <div class="todo-details">
-            <p>Buy milk, bread, and eggs from the store.</p>
-          </div>
-        </li>
-        <li>
-          <div class="todo-header">
-            <span>Read a book</span>
-            <i class="fas fa-trash trash-icon"></i>
-          </div>
-          <div class="todo-details">
-            <p>Finish reading the new novel I've started.</p>
-          </div>
-        </li>
-        <li>
-          <div class="todo-header">
-            <span>Exercise</span>
-            <i class="fas fa-trash trash-icon"></i>
-          </div>
-          <div class="todo-details">
-            <p>Go for a run in the park for 30 minutes.</p>
-          </div>
-        </li>
+        <For each={workouts().sort((a, b) => a.pos - b.pos)}>
+          {(workout, index) => (
+            <li>
+              <div class="todo-header">
+                <span>
+                  {workout.pos} - {workout.name}
+                </span>
+                <button>
+                  <i class="fas fa-trash trash-icon"></i>
+                </button>
+              </div>
+              <div class="todo-details">
+                <p>Buy milk, bread, and eggs from the store.</p>
+              </div>
+            </li>
+          )}
+        </For>
       </ul>
     </>
   );
