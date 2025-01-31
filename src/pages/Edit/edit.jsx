382e075e19aff10417cdccceeb461a205ad4561d
Khanh Nguyen <cammini@khanhs-mac-mini.lan>
2025-01-30 20:56:39 -0500

    update styles

--- src/pages/Edit/edit.jsx
+++ src/pages/Edit/edit.jsx
@@ -2,22 +2,35 @@ import "./Edit.css";
 const Edit = () => {
   return (
     <>
-    
-<ul class="todo-list">
-  <li>
-    <span>Buy groceries</span>
-    <i class="fas fa-trash trash-icon"></i>
-  </li>
-  <li>
-    <span>Read a book</span>
-    <i class="fas fa-trash trash-icon"></i>
-  </li>
-  <li>
-    <span>Exercise</span>
-    <i class="fas fa-trash trash-icon"></i>
-  </li>
-</ul>
-
+      <ul class="todo-list">
+        <li>
+          <div class="todo-header">
+            <span>Buy groceries</span>
+            <i class="fas fa-trash trash-icon"></i>
+          </div>
+          <div class="todo-details">
+            <p>Buy milk, bread, and eggs from the store.</p>
+          </div>
+        </li>
+        <li>
+          <div class="todo-header">
+            <span>Read a book</span>
+            <i class="fas fa-trash trash-icon"></i>
+          </div>
+          <div class="todo-details">
+            <p>Finish reading the new novel I've started.</p>
+          </div>
+        </li>
+        <li>
+          <div class="todo-header">
+            <span>Exercise</span>
+            <i class="fas fa-trash trash-icon"></i>
+          </div>
+          <div class="todo-details">
+            <p>Go for a run in the park for 30 minutes.</p>
+          </div>
+        </li>
+      </ul>
     </>
   );
 };
