0f60214d7d9f2544108b4e405814992235f718c3
Khanh Nguyen <khnguy3n@proton.me>
2025-02-01 04:04:37 -0500

    new button and toggle
    add new workout button and
    add listItem toggle behavior

--- src/pages/Edit/edit.jsx
+++ src/pages/Edit/edit.jsx
@@ -4,18 +4,24 @@ import EditForm from "./editForm.jsx";
 
 const Edit = () => {
   const [workouts, setWorkouts] = createSignal([
-    { name: "push ups", dur: 30, pos: 0 },
-    { name: "pull ups", dur: 20, pos: 11 },
-    { name: "plank", dur: 10, pos: 2 },
+    { name: "push ups", dur: 30 },
+    { name: "pull ups", dur: 20 },
+    { name: "plank", dur: 10 },
   ]);
+
+  const [activeWorkout, setActiveWorkout] = createSignal(-1);
+
   return (
     <>
+      <button>add new</button>
       <ul class="todo-list">
         <For each={workouts().sort((a, b) => a.pos - b.pos)}>
           {(workout, index) => (
-            <li>
+            <li
+              onClick={() => setActiveWorkout(index())}
+              classList={{ open: activeWorkout() === index() }}
+            >
               <div class="todo-header">
-                <div>{workout.pos}</div>
                 <span>{workout.name}</span>
               </div>
               <div class="todo-details">
