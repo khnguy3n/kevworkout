468bedb0e143db3fc0dad1c6dd1e193db9355604
Khanh Nguyen <khnguy3n@proton.me>
2025-02-04 22:53:13 -0500

    move start page to folder

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,10 @@ import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
 import "./index.css";
 import App from "./App.jsx";
-import Start from "./pages/start.jsx";
+import Start from "./pages/Start/start.jsx";
 import Edit from "./pages/Edit/edit.jsx";
 
 const root = document.getElementById("root");
-
-//render(() => <App />, root)
 
 const routes = [
   {
