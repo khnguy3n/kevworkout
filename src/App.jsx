4cb4993639f0ef59665c244f5f42751fe6203536
Khanh Nguyen <khnguy3n@proton.me>
2025-02-16 16:27:15 -0500

    change init page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import PWABadge from "./PWABadge.jsx";
 
 function App() {
   return (
-    <>
-      <a href="/edit">
-        <button>Edit</button>
-      </a>
-      <a href="/start">
-        <button>Start</button>
-      </a>
+    <div class="h-screen">
+      <div class="flex flex-col h-full gap-2">
+        <a
+          href="/edit"
+          class="flex items-center justify-center flex-1 w-full bg-mart-600 text-mooblu-200 rounded-lg shadow text-center"
+        >
+          <span class="text-[15vh]">Edit</span>
+        </a>
+        <a
+          href="/rest"
+          class="flex items-center justify-center flex-1 w-full bg-mart-600 text-mooblu-200 rounded-lg shadow text-center"
+        >
+          <span class="text-[15vh]">Rest</span>
+        </a>
+        <a
+          href="/start"
+          class="flex items-center justify-center flex-1 w-full bg-mart-600 text-mooblu-200 rounded-lg shadow text-center"
+        >
+          <span class="text-[15vh]">Start</span>
+        </a>
+      </div>
       <PWABadge />
-    </>
+    </div>
   );
 }
-
 export default App;
