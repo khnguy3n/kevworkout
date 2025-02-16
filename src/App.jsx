fd2068be3f4629bd930fd67ab3c24d8910ae40d2
Khanh Nguyen <khnguy3n@proton.me>
2025-02-15 19:51:51 -0500

    more formatting

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import PWABadge from "./PWABadge.jsx";
 function App() {
   return (
     <>
-      <a href="/edit"><button>Edit</button></a>
-      <a href="/start"><button>Start</button></a>
+      <a href="/edit">
+        <button>Edit</button>
+      </a>
+      <a href="/start">
+        <button>Start</button>
+      </a>
       <PWABadge />
     </>
   );
